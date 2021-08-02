73df50751415ab1e35f24dfbd52f2c289712b4ec
Aleksandr <kaktys2@narod.ru>
2021-08-02 21:57:21 +0300

    release

---
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+	
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,6 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SfgPetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
