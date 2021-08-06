d03f549f64b1aa29fea58b225cc44def43f21514
Aleksandr <kaktys2@narod.ru>
2021-08-06 14:04:54 +0300

    Update map impl to manage id. Closes #21

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -38,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(34L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(4L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
