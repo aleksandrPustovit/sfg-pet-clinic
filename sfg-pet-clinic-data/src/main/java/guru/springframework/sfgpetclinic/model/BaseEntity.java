21abf7068bbf22d9a6c1070bbbc7c47df104111f
Aleksandr <kaktys2@narod.ru>
2021-08-12 22:20:26 +0300

    Update Pwner entity with JPA annotation.Closes #31

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 
 	public Long getId() {
